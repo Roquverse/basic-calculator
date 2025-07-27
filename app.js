4d1bb8e9c26a4fcce94e33bf07f4f9472d86e5d1
Oluwadamilola <roqueverse@gmail.com>
2025-07-27 18:07:49 +0100

    second commit

--- app.js
+++ app.js
@@ -19,4 +19,32 @@ function div() {
   result.innerHTML = div;
 }
 
+function peri() {
+  let perimeter = (Number(num1.value) + Number(num2.value)) * 2;
+  result.innerHTML = perimeter;
+}
+
+function area() {
+  let area = Number(num1.value) * Number(num2.value);
+  result.innerHTML = area;
+}
+
+function temp() {
+  let inputValue = Number(document.querySelector("#value").value);
+  let cel = document.querySelector("#celsius");
+  let fa = document.querySelector("#fah");
+
+  if (cel.checked) {
+    let celsiusCon = `${(inputValue * 9) / 5 + 32}°C`;
+    result.innerHTML = celsiusCon;
+  }
+  if (fa.checked) {
+    let fahCon = `${(inputValue - 32) / 1.8}°F`;
+    result.innerHTML = fahCon;
+  }
+}
 // Add buttons for Area and Perimeter and create the logic for both buttons
+//  git add .
+//  git commit -m "second commit"
+//  git remote add origin https://github.com/Roquverse/basic-calculator.git
+//  git push -u origin main
